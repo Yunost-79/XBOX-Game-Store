4c72a65abc6f0ada5044c5f745a5359d6e9c563c
Yunos_T <yunoshevv2000@gmail.com>
2022-10-17 20:16:23 +0300

    add offers block

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
 const buyLink = document.querySelectorAll('.buy-game-link');
+const buyItem = document.querySelectorAll('.offers-block');
 
 buyLink.forEach((e) => {
   e.addEventListener('click', () => {
-    alert('You buy it!!!');
+    alert('You buught game!!!');
   });
 });
+
+buyItem.forEach((e)=>{
+  e.addEventListener('click', () =>{
+    alert('You buught game!!!, again');
+
+  })
+})
 
 const links = document.querySelectorAll('a');
 
