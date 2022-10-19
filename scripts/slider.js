f4984ad54914d0332c1ed978affb222012a90d67
Yunos_T <yunoshevv2000@gmail.com>
2022-10-19 15:29:10 +0300

    add blocks: Coming Soon 12.000+ gamers satisfied

--- scripts/slider.js
+++ scripts/slider.js
@@ -17,15 +17,15 @@ let swiper1 = new Swiper('.mySwiper-1', {
   rewind: true,
 });
 
-var swiper = new Swiper('.mySwiper-2', {
+let swiper2 = new Swiper('.mySwiper-2', {
   slidesPerView: 1,
   spaceBetween: 10,
   breakpoints: {
-    500:{
+    500: {
       slidesPerView: 2,
       spaceBetween: 10,
     },
-    768:{
+    768: {
       slidesPerView: 3,
       spaceBetween: 10,
     },
@@ -33,7 +33,6 @@ var swiper = new Swiper('.mySwiper-2', {
       slidesPerView: 4,
       spaceBetween: 10,
     },
-
   },
   autoplay: {
     delay: 3500,
@@ -46,9 +45,44 @@ var swiper = new Swiper('.mySwiper-2', {
     el: '.swiper-pagination',
     clickable: true,
   },
-  
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+let swiper3 = new Swiper('.mySwiper-3', {
+  slidesPerView: 'auto',
+  slidesPerView: 1,
+  spaceBetween: 5,
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 27,
+
+    },
+  },
+  autoHeight: true,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 0,
+    depth: 50,
+    scale: 0.9,
+    slideShadows: false,
+  },
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
